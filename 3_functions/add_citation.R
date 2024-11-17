3e6a2a48f09b9faa38c11d2de3d3d7cc7db29ee3
Christopher.Donovan <christopher.donovan@uvm.edu>
2024-11-16 19:46:32 -0500

    spatial wrangling for environment page

--- 3_functions/add_citation.R
+++ 3_functions/add_citation.R
@@ -1,16 +1,28 @@
 # get citation
 # quick function to paste together source with URL to get APA style citation
 
+pacman::p_load(
+  dplyr,
+  lubridate
+)
+
 add_citation <- function(df, 
                          source_col = source, 
-                         url_col = url) {
+                         url_col = url,
+                         access_date = NULL) {
+  if (is.null(access_date)) {
+    date_of_access <- format(Sys.Date(), "%B %d, %Y")
+  } else {
+    date_of_access <- format(ymd(access_date), "%B %d, %Y")
+  }
+  
   df %>%
     mutate(citation = paste0(
       {{ source_col }},
       ', <',
       {{ url_col }},
       '>, accessed on ',
-      format(Sys.Date(), "%B %d, %Y"),
+      date_of_access,
       '.'
     ))
 }
